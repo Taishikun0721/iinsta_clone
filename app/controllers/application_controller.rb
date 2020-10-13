f0e8bb1d648a86813867f9be13436a22f9f60df6
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-10-13 22:25:46 +0900

    フラッシュメッセージでnoticeとalert以外も対応させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # フラッシュメッセージのキーを許可する表示設定。Bootstrapはデフォルトではnoticeとsuccessしか許可していないらしい。
+  add_flash_types :success, :info, :warning, :danger
 end
