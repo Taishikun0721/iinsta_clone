6a8a17209bf0e2591d78a862821ac519ba3972e9
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-10-19 08:21:27 +0900

    postsコントローラー対するルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#new'
 
   resources :users, only: [:new, :create]
+  resources :posts
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
