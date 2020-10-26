3527c39cdc4ec66ba2f6c5a3a5487699d92daaa0
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-10-26 22:46:41 +0900

    annotateのコメント位置を前に持ってきた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  salt             :string(255)
+#  username         :string(255)      not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
 class User < ApplicationRecord
   authenticates_with_sorcery!
   # 紐づいているユーザーが削除された場合、紐づいているpostも自動的に全て削除する。
@@ -15,20 +32,3 @@ class User < ApplicationRecord
     id == object.user_id
   end
 end
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id               :bigint           not null, primary key
-#  crypted_password :string(255)
-#  email            :string(255)      not null
-#  salt             :string(255)
-#  username         :string(255)      not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email  (email) UNIQUE
-#
