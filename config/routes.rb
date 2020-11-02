a639f4b30997dc65afd40e83d371666ec3f7fb94
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-11-02 12:45:11 +0900

    shallowオプションを使ってルーティングを記載

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'users#new'
 
   resources :users, only: [:new, :create]
-  resources :posts
+  # shallowルーティングを使用すると、必ずネストが必要な(紐づけられたものを表示or作成するから)indexとcreate以外のネストを無くしてくれる。
+  resources :posts, shallow: true do
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
+  end
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
