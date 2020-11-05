d5a89c71c037f7b6213ae0baeb0a8ce78bf16c68
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-11-06 08:39:42 +0900

    不要なupとdownメソッドをchangeに置き換え

--- db/migrate/20201104120330_add_likes_count_to_posts.rb
+++ db/migrate/20201104120330_add_likes_count_to_posts.rb
@@ -1,9 +1,5 @@
 class AddLikesCountToPosts < ActiveRecord::Migration[5.2]
-  def self.up
+  def change
     add_column :posts, :likes_count, :integer, null: false, default: 0
-  end
-
-  def self.down
-    remove_column :posts, :likes_count
   end
 end
