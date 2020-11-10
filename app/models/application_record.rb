99362ade8b47c9df9995ebd515c4785a2a1e4f42
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-11-10 23:35:14 +0900

    scopeのrecentメソッドをリファクタリング

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,6 @@ class ApplicationRecord < ActiveRecord::Base
   # コメントインスタンスにも使用したかったのでApplication_Recordに移動
   # 引数を渡さない場合、全userが降順に並べ替えられるだけにして、引数を渡した場合は、limitが付与される
   scope :recent, -> (count = nil) do
-    return order(created_at: :desc) unless count
-    order(created_at: :desc).limit(count)
+    count.present? ? order(created_at: :desc).limit(count) : order(created_at: :desc)
   end
 end
